5e02619f452d50cb2c7fa0d121eff42c13117bc3
Winter Alava <walava@espol.edu.ec>
2020-07-16 16:51:17 -0500

    Se añadio el telefono como info de usuario

--- app/src/main/java/com/grupo1/grupo1_ta3_iniciosesionfirebase/PerfilUsuario.java
+++ app/src/main/java/com/grupo1/grupo1_ta3_iniciosesionfirebase/PerfilUsuario.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 
 public class PerfilUsuario extends AppCompatActivity {
 
-    TextView txt_id, txt_name, txt_email;
+    TextView txt_id, txt_name, txt_email,txt_phone;
     ImageView imv_photo;
     Button btn_logout;
     @Override
@@ -29,9 +29,11 @@ public class PerfilUsuario extends AppCompatActivity {
         txt_name = findViewById(R.id.txt_nombre);
         txt_email = findViewById(R.id.txt_correo);
         imv_photo = findViewById(R.id.imv_foto);
+        txt_phone = findViewById(R.id.txt_telefono);
         txt_id.setText(info_user.get("user_id"));
         txt_name.setText(info_user.get("user_name"));
         txt_email.setText(info_user.get("user_email"));
+        txt_phone.setText(info_user.get("user_phone"));
         String photo = info_user.get("user_photo");
         Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
     }
